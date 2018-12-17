5d0606d619754af486c0583aa05c4e15140ab7e0
nicklasmoeller <hello@nicklasmoeller.com>
2018-12-17 21:02:08 +0100

    Add Vec3 util

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate lodepng;
 mod utils;
 
 use utils::rgb::RGB;
+use utils::vec3::Vec3;
 
 fn main() {
     const WIDTH: usize = 800;
@@ -12,12 +13,10 @@ fn main() {
 
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
-            let r_point = x as f32 / WIDTH as f32;
-            let g_point = y as f32 / HEIGHT as f32;
-            let b_point = 0.2f32;
-            let r = (255.99 * r_point) as u8;
-            let g = (255.99 * g_point) as u8;
-            let b = (255.99 * b_point) as u8;
+            let color = Vec3::new(x as f32 / WIDTH as f32, y as f32 / HEIGHT as f32, 0.2f32);
+            let r = (255.99 * color.x) as u8;
+            let g = (255.99 * color.y) as u8;
+            let b = (255.99 * color.z) as u8;
             data[x + (HEIGHT - y - 1) * WIDTH] = RGB { r, g, b };
         }
     }
